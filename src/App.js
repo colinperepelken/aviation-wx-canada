315709e21967e9e372d10cde03ba71b2de90a5f7
colinbernard <colinjbernard@hotmail.com>
2019-01-31 23:02:14 -0800

    Combined header and navigation components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import MetarForm from './components/MetarForm';
 import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />  
-        <Navigation/>
+        <Header />
       </div>
     );
   }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import './Header.css';
+import { Navbar, Nav, NavDropDown, Form, FormControl, Button} from 'react-bootstrap';
 
 class Header extends Component {
   render() {
     return <header>
       <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-      integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-      crossorigin="anonymous"/>
-    </header>
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+        crossorigin="anonymous"/>
+      <Navbar bg="primary" variant="dark">
+      <Navbar.Brand href="#home">Aviation WX Canada</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link className="active" href="#home">Home</Nav.Link>
+        <Nav.Link href="#">About</Nav.Link>
+        <Nav.Link href="#">Weather</Nav.Link>
+        <Nav.Link href="#">File a Flight Plan</Nav.Link>
+      </Nav>
+      <Nav className="navbar-right">
+        <Nav.Link href="#">Sign Up</Nav.Link>
+        <Nav.Link href="#">Login</Nav.Link>
+      </Nav>
+    </Navbar>;
+  </header>
   }
 }
 
